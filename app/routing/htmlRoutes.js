2e740fbfe74b23179413fbc5208cf59b89ed7e39
mekaknepley <mekaknepley@gmail.com>
2017-08-15 17:48:30 -0400

    heroku2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
     // ---------------------------------------------------------------------------
 
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/surveyno.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
 
