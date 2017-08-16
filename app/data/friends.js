a43048095b9a2c05b237b45993bd6f159abd5ad6
mekaknepley <mekaknepley@gmail.com>
2017-08-16 12:53:52 -0400

    heroku2

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,7 +8,7 @@
 var friendsArray = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"/images/random-user-imageM45.jpg",
         "scores":[
             5,
             1,
